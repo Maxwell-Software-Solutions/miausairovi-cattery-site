151888dd830cadb52def7d116fc84c8ef5f028b4
Mantas Brasiunas <hex0@live.com>
2025-10-22 14:29:43 +0100

    fix: Comment out width attribute for logo image in Navigation component

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -22,6 +22,7 @@ const Navigation = memo(() => {
               src={logo}
               alt="Miausairovi Cattery - British Shorthair Breeder Peterborough UK"
               className="h-12 md:h-14"
+              // width="56"
               height="56"
               loading="eager"
             />
